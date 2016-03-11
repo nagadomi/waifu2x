55906747830c867da3a32fbdfa87d27ca94fbd90
nagadomi <nagadomi@nurs.or.jp>
2016-03-12 08:47:14 +0900

    Fix NaN in PSNRCriterion

--- lib/PSNRCriterion.lua
+++ lib/PSNRCriterion.lua
@@ -10,7 +10,7 @@ function PSNRCriterion:updateOutput(input, target)
    self.image:clamp(0.0, 1.0)
    self.diff:resizeAs(self.image):copy(self.image)
    
-   local mse = self.diff:add(-1, target):pow(2):mean()
+   local mse = math.max(self.diff:add(-1, target):pow(2):mean(), (0.1/255)^2)
    self.output = 10 * math.log10(1.0 / mse)
    return self.output
 end
