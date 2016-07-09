6761b852b96aadbe646c83de8e25dc3c9525a0d2
nagadomi <nagadomi@nurs.or.jp>
2016-07-09 15:42:50 +0900

    Update images

--- images/gen.sh
+++ images/gen.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 th waifu2x.lua -m scale -i images/miku_small.png -o images/miku_small_waifu2x.png
-th waifu2x.lua -noise_level 2 -m noise_scale -i images/miku_small_noisy.png -o images/miku_small_noisy_waifu2x.png
-th waifu2x.lua -noise_level 2 -m noise -i images/miku_noisy.png -o images/miku_noisy_waifu2x.png
-th waifu2x.lua -noise_level 2 -m noise -i images/lena.png -o images/lena_waifu2x.png
-th waifu2x.lua -m scale -model_dir models/ukbench -i images/lena.png -o images/lena_waifu2x_ukbench.png
+th waifu2x.lua -m noise_scale -noise_level 1 -i images/miku_small_noisy.png -o images/miku_small_noisy_waifu2x.png
+th waifu2x.lua -m noise_scale -noise_level 1 -i images/miku_CC_BY-NC_noisy.jpg -o images/miku_CC_BY-NC_noisy_waifu2x.png
+th waifu2x.lua -m noise -noise_level 2 -i images/miku_noisy.png -o images/miku_noisy_waifu2x.png
+th waifu2x.lua -m noise -noise_level 2 -i images/lena.png -o images/lena_waifu2x_art_noise2.png
+
+th waifu2x.lua -m scale -i images/tone.png -o images/tone_waifu2x.png
+
+th waifu2x.lua -m scale -model_dir models/upconv_7/photo -i images/lena.png -o images/lena_waifu2x_photo.png
+th waifu2x.lua -m noise_scale -noise_level 3 -model_dir models/upconv_7/photo -i images/lena.png -o images/lena_waifu2x_photo_noise3.png
+th waifu2x.lua -m scale -model_dir models/upconv_7/photo -i images/city.jpg -o images/city_waifu2x_photo.png
