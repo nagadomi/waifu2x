382d493514c879e9aaef79c8f7e60dc2710a8602
nagadomi <nagadomi@nurs.or.jp>
2016-07-27 22:55:56 +0900

    no confidence change for #128

--- lib/w2nn.lua
+++ lib/w2nn.lua
@@ -7,18 +7,17 @@ local function load_cunn()
    require 'cunn'
 end
 local function load_cudnn()
-   require 'cudnn'
-   cudnn.benchmark = true
+   cudnn = require('cudnn')
 end
 if w2nn then
    return w2nn
 else
+   w2nn = {}
    local state, ret = pcall(load_cunn)
    if not state then
       error("Failed to load CUDA modules. Please check the CUDA Settings.\n---\n" .. ret)
    end
    pcall(load_cudnn)
-   w2nn = {}
 
    function w2nn.load_model(model_path, force_cudnn)
       local model = torch.load(model_path, "ascii")
