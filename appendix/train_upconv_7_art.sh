93511a2a9f01f1d6bdcbb767ca6ebd2b498c219a
nagadomi <nagadomi@nurs.or.jp>
2016-07-30 05:16:02 +0900

    Update upconv_7/art/scale2.0x model; Fix mesh-artifact

--- appendix/train_upconv_7_art.sh
+++ appendix/train_upconv_7_art.sh
@@ -4,7 +4,9 @@
 th convert_data.lua -max_training_image_size 1600
 
 # scale
-th train.lua -save_history 1 -scale 2 -model upconv_7 -method scale -model_dir models/test/upconv_7_rev5 -downsampling_filters "Box,Sinc" -test query/pixel-art-small.png -backend cudnn -thread 4 -oracle_rate 0.05
+# after train, I selected `scale2.0x_model.86-1.t7`, because seemingly this model is best mesh-artifact free model. Maybe visual testing is required.
+# ref: https://github.com/nagadomi/waifu2x/issues/125
+th train.lua -save_history 1 -scale 2 -model upconv_7 -method scale -model_dir models/test/upconv_7_rev5 -downsampling_filters "Box,Sinc" -test query/pixel-art-small.png -backend cudnn -thread 4 -oracle_rate 0.0 -inner_epoch 2 -epoch 100
 
 # noise_scale
 
